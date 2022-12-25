6fa92a59f54fd0584d35aa6882caef05704b4b6c
muy <redemperor@yeah.net>
2022-12-25 10:45:35 +0800

    Scanner小数

---
+++ src/main/java/practice/characterString/ScannerDecimals.java
@@ -0,0 +1,14 @@
+package practice.characterString;
+
+import java.util.Scanner;
+
+public class ScannerDecimals {
+    public static void main(String[] args) {
+//        创建对象
+        Scanner sc =new Scanner(System.in);
+        System.out.println("请输入一个小数");
+        double v = sc.nextDouble();
+        System.out.println(v);
+
+    }
+}
